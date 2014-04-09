35c3446479d9a96a36da5e8886f65de2af943d46
Alex Miller <millerdevel@gmail.com>
2014-04-09 23:51:30 +0000

    Add gmp to travis.sh

--- .travis.sh
+++ .travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ATS2_HEAD=00b8bbbcf8bcc6f320728a667ec3ca511aa50cac
 
-sudo apt-get install -qq git
+sudo apt-get install -qq git libgmp3-dev
 
 git clone https://github.com/thisismiller/literate.git literate
 
