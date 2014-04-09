51be7270166ff27ac0145fa50e1020b3111d770b
Alex Miller <millerdevel@gmail.com>
2014-04-09 23:48:51 +0000

    Attempt #1 at travis-ci.

---
+++ .travis.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ATS2_HEAD=00b8bbbcf8bcc6f320728a667ec3ca511aa50cac
+
+sudo apt-get install -qq git
+
+git clone https://github.com/thisismiller/literate.git literate
+
+git clone git://git.code.sf.net/p/ats2-lang/code postiats
+
+pushd postiats
+git checkout $ATS2_HEAD
+./autogen.sh || true
+./configure
+make -j1
+popd
+
+export PATSHOME=$(pwd)/postiats
+export UNLIT=literate/bin/unlit
+export ATSOPT=postiats/bin/patsopt
+export ATSCC=postiats/bin/patscc
+make
