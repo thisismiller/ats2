d2bce6fe09a3c060ed34060c43a103ac97f4ee8e
Alex Miller <millerdevel@gmail.com>
2014-04-19 22:01:43 +0000

    Use githwxi/ATS-Postiats as upstream and do a full bootstrap
    Github upstream seems to be the authoritative upstream, so we should
    switch over.  It looks like CBOOT wasn't dropped completely, so we might
    be able to shorten this compile process in the future, but this does
    serve as a good record of how to do a full bootstrap.

--- .travis.sh
+++ .travis.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
-sudo apt-get install -qq git libgmp3-dev
+sudo apt-get install -qq git svn libgmp3-dev
 
 # Git complains if it isn't configured
 git config --global user.email "travis@localhost.localdomain"
 git config --global user.name "travis"
 
-git clone https://github.com/thisismiller/literate.git literate
+svn export svn://svn.code.sf.net/p/ats-lang/code/trunk anariats
+svn export svn://svn.code.sf.net/p/ats-lang/code/bootstrap/anairiats anariats/bootstrap0
 
-git clone git://git.code.sf.net/p/ats2-lang/code postiats
+pushd anariats
+  make
+popd
+
+export ATSHOME=$(pwd)/anariats
+# This should be stable given that development is now focused on ATS2
+export ATSHOMERELOC=ATS-0.2.11
+
+git clone https://github.com/githwxi/ATS-Postiats.git postiats
 
 pushd postiats
-git reset --hard $ATS2_COMMIT
+#git reset --hard $ATS2_COMMIT
 for file in $(ls ../.ats_patches); do
   git am ../.ats_patches/$file
 done
-./autogen.sh || true
+./autoreconf -i || true
 ./configure
-make -j1
+make -f Makefile_devl
 popd
+
+git clone https://github.com/thisismiller/literate.git literate
 
 export PATSHOME=$(pwd)/postiats
 export UNLIT=literate/bin/unlit
